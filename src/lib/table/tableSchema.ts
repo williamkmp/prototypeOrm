6537c40f0265723e0de413631cc62168b1d52147
William Kurnia Mulyadi Putra <williamkurnia123@gmail.com>
2022-03-13 13:48:38 +0700

    added getCreateQuery() not implemented

--- src/lib/table/tableSchema.ts
+++ src/lib/table/tableSchema.ts
@@ -1,9 +1,15 @@
 import { ColumnSchema } from "../columns/column.js"
 
+
+/**
+ * class to describe a table from client schema
+ * contains table name, columnSchemas, tableName
+ * and CREATE TABLE query
+ */
 export class TableSchema{
     name: string;
     primaryColumn: ColumnSchema | null;
-    columnsArray: Array<ColumnSchema>;
+    columnsArray: Array<ColumnSchema>; //including the primary key
       
     constructor(name: string, columnSchemas: Array<ColumnSchema>){
         this.name = name;
@@ -15,4 +21,14 @@ export class TableSchema{
         });
         this.columnsArray = columnSchemas;
     }
+
+    /**
+     * create MySQL query for creating the table
+     * including the constraint (oncreate constraint)
+     * @returns {string} sql query to create table
+     */
+    getCreateQuery(): string{
+		//TODO: implement create query from this.columnsArray
+		return "";
+	}
 }
